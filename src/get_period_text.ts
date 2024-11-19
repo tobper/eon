e02a9de0f311ec9f371d6a20fafe92857fdec9c2
Tobias Persson <tobper@gmail.com>
2024-11-19 14:38:17 +0100

    Remove year from period texts when it is the current year

--- src/get_period_text.ts
+++ src/get_period_text.ts
@@ -2,21 +2,37 @@ import type { Period } from './create_period.js';
 import { to_date } from './to_date.js';
 
 const locale = 'en';
-const text_long_format_options: Intl.DateTimeFormatOptions = { year: 'numeric', month: 'long' };
-const text_short_format_options: Intl.DateTimeFormatOptions = { month: 'short', day: 'numeric' };
-const text_short_with_year_format_options: Intl.DateTimeFormatOptions = { year: 'numeric', month: 'short', day: 'numeric' };
 
 export function get_period_text(
 	period: Period
 ) {
+	const current_year = new Date().getFullYear();
+	const include_year =
+		period.first_day.year !== current_year ||
+		period.last_day.year !== current_year;
+
 	if (period.first_day.day === 1) {
 		// November 2023
-		return to_date(period.first_day).toLocaleDateString(locale, text_long_format_options);
+		return to_date(period.first_day)
+			.toLocaleDateString(locale, {
+				year: include_year ? 'numeric' : undefined,
+				month: 'long'
+			});
 	}
 
 	// Nov 25 - Dec 24 2023
-	const from = to_date(period.first_day).toLocaleDateString(locale, text_short_format_options);
-	const to = to_date(period.last_day).toLocaleDateString(locale, text_short_with_year_format_options);
+	const from = to_date(period.first_day)
+		.toLocaleDateString(locale, {
+			month: 'short',
+			day: 'numeric'
+		});
+
+	const to = to_date(period.last_day)
+		.toLocaleDateString(locale, {
+			year: include_year ? 'numeric' : undefined,
+			month: 'short',
+			day: 'numeric'
+		});
 
 	return `${from} - ${to}`;
 }
--- tests/get_period_text.spec.ts
+++ tests/get_period_text.spec.ts
@@ -3,13 +3,29 @@ import { create_period } from '../src/create_period';
 import { get_period_text } from '../src/get_period_text';
 
 describe('get_period_text()', () => {
-	test('Returns single month and year when period starts on the first', () => {
-		const period = create_period(2023, 1, 1);
-		expect(get_period_text(period)).toBe('January 2023');
+	describe('When period starts on the first of the month', () => {
+		test('Returns single month and year', () => {
+			const period = create_period(2023, 1, 1);
+			expect(get_period_text(period)).toBe('January 2023');
+		});
+
+		test('Omits year if period is current year', () => {
+			const year = new Date().getFullYear();
+			const period = create_period(year, 1, 1);
+			expect(get_period_text(period)).toBe(`January`);
+		});
 	});
 
-	test('Returns months and days of first and last day when period does not start on the first', () => {
-		const period = create_period(2023, 1, 15);
-		expect(get_period_text(period)).toBe('Jan 15 - Feb 14, 2023');
+	describe('When period does not starts on the first of the month', () => {
+		test('Returns months and days of first and last day', () => {
+			const period = create_period(2023, 1, 15);
+			expect(get_period_text(period)).toBe('Jan 15 - Feb 14, 2023');
+		});
+
+		test('Omits year if period is current year', () => {
+			const year = new Date().getFullYear();
+			const period = create_period(year, 1, 15);
+			expect(get_period_text(period)).toBe('Jan 15 - Feb 14');
+		});
 	});
 });
